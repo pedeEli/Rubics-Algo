4bc00a4dcd4441ba2acb32375e604fd9c6d42f6c
pedeEli <74102959+pedeEli@users.noreply.github.com>
2022-06-30 02:51:46 +0200

    added algorithms for all no edges oriented

--- src/lib/OLLCubes.ts
+++ src/lib/OLLCubes.ts
@@ -51,70 +51,6 @@ const oll: Record<OLLSections, Record<OLLType, OLLProps>> = {
             "bottomRight": 2
         }
     },
-    "No Edges Oriented": {
-        "OLL 1": {
-            "topLeft": 1,
-            "top": 1,
-            "topRight": 2,
-            "right": 1,
-            "bottomRight": 1,
-            "bottom": 1,
-            "bottomLeft": 2,
-            "left": 1
-        },
-        "OLL 2": {
-            "topLeft": 1,
-            "top": 1,
-            "topRight": 1,
-            "right": 1,
-            "bottomRight": 2,
-            "bottom": 1,
-            "bottomLeft": 2,
-            "left": 1
-        },
-        "OLL 3": {
-            "topLeft": 2,
-            "top": 1,
-            "topRight": 2,
-            "right": 1,
-            "bottom": 1,
-            "bottomLeft": 2,
-            "left": 1
-        },
-        "OLL 4": {
-            "topLeft": 1,
-            "top": 1,
-            "right": 1,
-            "bottomRight": 1,
-            "bottom": 1,
-            "bottomLeft": 1,
-            "left": 1
-        },
-        "OLL 17": {
-            "top": 1,
-            "topRight": 1,
-            "right": 1,
-            "bottom": 1,
-            "bottomLeft": 2,
-            "left": 1
-        },
-        "OLL 18": {
-            "topLeft": 1,
-            "top": 1,
-            "right": 1,
-            "bottom": 1,
-            "bottomLeft": 2,
-            "left": 1
-        },
-        "OLL 19": {
-            "top": 1,
-            "right": 1,
-            "bottomRight": 1,
-            "bottom": 1,
-            "bottomLeft": 2,
-            "left": 1
-        }
-    },
     "P Shapes": {
         "OLL 31": {
             "topLeft": 2,
@@ -409,6 +345,70 @@ const oll: Record<OLLSections, Record<OLLType, OLLProps>> = {
             "right": 1,
             "bottom": 1,
             "bottomLeft": 2
+        }
+    },
+    "No Edges Oriented": {
+        "OLL 1": {
+            "topLeft": 1,
+            "top": 1,
+            "topRight": 2,
+            "right": 1,
+            "bottomRight": 1,
+            "bottom": 1,
+            "bottomLeft": 2,
+            "left": 1
+        },
+        "OLL 2": {
+            "topLeft": 2,
+            "top": 1,
+            "topRight": 1,
+            "right": 1,
+            "bottomRight": 1,
+            "bottom": 1,
+            "bottomLeft": 2,
+            "left": 1
+        },
+        "OLL 3": {
+            "topLeft": 2,
+            "top": 1,
+            "topRight": 2,
+            "right": 1,
+            "bottomRight": 2,
+            "bottom": 1,
+            "left": 1
+        },
+        "OLL 4": {
+            "topLeft": 1,
+            "top": 1,
+            "topRight": 1,
+            "right": 1,
+            "bottom": 1,
+            "bottomLeft": 1,
+            "left": 1
+        },
+        "OLL 17": {
+            "top": 1,
+            "topRight": 2,
+            "right": 1,
+            "bottom": 1,
+            "bottomLeft": 1,
+            "left": 1
+        },
+        "OLL 18": {
+            "top": 1,
+            "right": 1,
+            "bottomRight": 2,
+            "bottom": 1,
+            "bottomLeft": 1,
+            "left": 1
+        },
+        "OLL 19": {
+            "top": 1,
+            "right": 1,
+            "bottomRight": 1,
+            "bottom": 1,
+            "bottomLeft": 2,
+            "left": 1
         }
     }
 }
