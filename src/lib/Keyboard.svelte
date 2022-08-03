6cd0d4cd8e8a1439fd0b52f72e87151131795dab
pedeEli <74102959+pedeEli@users.noreply.github.com>
2022-08-03 22:23:39 +0200

    made keyboard smaller on smaller screens

--- src/lib/Keyboard.svelte
+++ src/lib/Keyboard.svelte
@@ -64,35 +64,35 @@
             <button use:Ripple={{surface: true}} class="{toggleKey}" class:smui-button--color-secondary={side !== letter} on:click={setSide(letter)}>{letter}</button>
         {/each}
 
-        <button use:Ripple={{surface: true}} disabled={disablePrevious} class="{actionKey}" on:click={previousHandler}>
-            <Icon component={Svg} viewBox="0 0 24 24">
+        <button use:Ripple={{surface: true}} disabled={disablePrevious} class="{actionKey} action" on:click={previousHandler}>
+            <Icon component={Svg} viewBox="0 0 24 24" style="height: var(--icon-height);">
                 <path fill="currentColor" d={mdiChevronLeft}/>
             </Icon>
         </button>
-        <button use:Ripple={{surface: true}} class="{actionKey}" on:click={nextHandler}>
-            <Icon component={Svg} viewBox="0 0 24 24">
+        <button use:Ripple={{surface: true}} class="{actionKey} action" on:click={nextHandler}>
+            <Icon component={Svg} viewBox="0 0 24 24" style="height: var(--icon-height);">
                 <path fill="currentColor" d={mdiChevronRight}/>
             </Icon>
         </button>
-        <button use:Ripple={{surface: true}} disabled={disableDelete} class="{actionKey}" on:click={delHandler}>
-            <Icon component={Svg} viewBox="0 0 24 24">
+        <button use:Ripple={{surface: true}} disabled={disableDelete} class="{actionKey} action" on:click={delHandler}>
+            <Icon component={Svg} viewBox="0 0 24 24" style="height: var(--icon-height);">
                 <path fill="currentColor" d={mdiDelete}/>
             </Icon>
         </button>
-        <button use:Ripple={{surface: true}} disabled={disableGroup} class="{actionKey}" on:click={groupHandler}>
-            <Icon component={Svg} viewBox="0 0 24 24">
+        <button use:Ripple={{surface: true}} disabled={disableGroup} class="{actionKey} action" on:click={groupHandler}>
+            <Icon component={Svg} viewBox="0 0 24 24" style="height: var(--icon-height);">
                 <path fill="currentColor" d={mdiCodeBrackets}/>
             </Icon>
         </button>
         <div class="insert smui-button__group">
             <button use:Ripple={{surface: true}} disabled={disableInsertLeft} class="{actionKey} insert-left" on:click={insertLeftHandler}>
-                <Icon component={Svg} viewBox="0 0 24 24">
+                <Icon component={Svg} viewBox="0 0 24 24" style="height: var(--icon-height);">
                     <path fill="currentColor" d={mdiChevronLeft}/>
                 </Icon>
             </button>
             <div class="insert-text">Insert</div>
             <button use:Ripple={{surface: true}} disabled={disableInsertRight} class="{actionKey} insert-right" on:click={insertRightHandler}>
-                <Icon component={Svg} viewBox="0 0 24 24">
+                <Icon component={Svg} viewBox="0 0 24 24" style="height: var(--icon-height);">
                     <path fill="currentColor" d={mdiChevronRight}/>
                 </Icon>
             </button>
@@ -101,7 +101,7 @@
 </div>
 
 
-<style>
+<style lang="scss">
     .keyboard {
         position: fixed;
         bottom: 0;
@@ -111,30 +111,26 @@
         justify-content: center;
         font-size: 1.2rem;
         z-index: 10;
+        background-color: var(--clr-surface);
     }
     .grid {
         width: min(25rem, 100%);
         display: grid;
         grid-template-columns: repeat(6, 1fr);
+        grid-auto-rows: 1fr;
         gap: .3rem;
-        background-color: var(--clr-surface);
-        padding: 4px;
-        padding-bottom: 0;
-        border-radius: 4px;
-    }
-    @media (max-width: 315px) {
-        .grid {
-            gap: .1rem;
-        }
+        padding-block: 6px;
+        padding-inline: 4px;
+        --icon-height: 2em;
+        aspect-ratio: 6 / 5;
     }
     .key {
-        aspect-ratio: 1 / 1;
         height: auto;
         min-width: auto;
         padding: 0;
+        text-transform: none;
     }
     .long {
-        aspect-ratio: 3 / 1;
         grid-column: span 3;
     }
     .active {
@@ -143,18 +139,20 @@
     .insert {
         display: grid;
         grid-template-columns: 50% 50%;
-        grid-template-rows: auto;
         grid-column: span 2;
+        user-select: none;
     }
     .insert-left {
         grid-column: 1 / 2;
         grid-row: 1 / 2;
         padding-right: .7em;
+        height: 100%;
     }
     .insert-right {
         grid-column: 2 / 3;
         grid-row: 1 / 2;
         padding-left: .7em;
+        height: 100%;
     }
     .insert-text {
         grid-column: 1 / 3;
@@ -165,5 +163,27 @@
         justify-content: center;
         pointer-events: none;
         transform: translateY(-5%);
+        height: 100%;
+        // background-color: red;
     }
+
+    @mixin mq($query, $value) {
+        @media (#{$query}: $value) {
+            .grid {
+                grid-template-columns: repeat(12, 1fr);
+                aspect-ratio: 12 / 4.5;
+                --icon-height: 1.6rem;
+            }
+            .insert {
+                grid-column: span 4;
+            }
+            .action {
+                grid-column: span 2;
+                
+            }
+        }
+    }
+
+    @include mq(max-width, 450px);
+    @include mq(max-height, 500px);
 </style>
